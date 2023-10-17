68827117e371e3e27def6b34bf93731e22dcdd36
fjkoki <fjkoki>
2023-10-18 00:02:31 +0300

    Add condition for test run only if pandoc is available

--- test_debian_news_to_markdown.py
+++ test_debian_news_to_markdown.py
@@ -1,12 +1,15 @@
 from debian_news_to_markdown import read_debian_news_with_pandoc_and_write_to_markdown
-from unittest import TestCase, main
+from unittest import TestCase, main, skipUnless
 from unittest.mock import MagicMock
+import shutil
 
 class TestDebianNewsToMarkdown(TestCase):
+    @skipUnless(shutil.which('pandoc'),'/usr/bin/pandoc')
     def test_read_debian_news_with_pandoc_and_write_to_markdown(self):
         actual_return_code = read_debian_news_with_pandoc_and_write_to_markdown()
         self.assertIsNone(actual_return_code)
 
+    @skipUnless(shutil.which('pandoc'),'/usr/bin/pandoc')
     def test_read_debian_news_with_pandoc_and_write_to_markdown_exception(self):
         read_debian_news_with_pandoc_and_write_to_markdown = MagicMock(side_effect=Exception("Something unexpected happened. Please try again"))
         with self.assertRaises(Exception):
